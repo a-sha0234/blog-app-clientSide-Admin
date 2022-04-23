641c744ac1b91d113ba421aa0c8d9300970f6cee
a-sha0234 <asharma_31@hotmail.com>
2022-04-23 23:46:36 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
 
 import Navbar from "./components/Navbar";
+import Posts from "./pages/posts";
 
 import Login from "./pages/Login";
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
   return (
     <BrowserRouter>
       <div className="App">
         {" "}
-        <Navbar />
+        <Navbar setIsLoggedIn={setIsLoggedIn} />
       </div>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/posts" element={<Posts />} />
       </Routes>
     </BrowserRouter>
   );
