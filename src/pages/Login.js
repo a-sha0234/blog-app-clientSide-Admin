b7ec6c1e28e44063f7fce89071762b3462cd6af1
a-sha0234 <asharma_31@hotmail.com>
2022-04-23 23:44:49 +0100

    make post request to the api, handle form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,35 +1,80 @@
 import { Button, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
 
 import React from "react";
 
-export default function Login() {
-  const [formData, setFormData] = React.useState({});
-  const [username, setUsername] = React.useState({});
-  const [password, setPassword] = React.useState({});
+export default function Login(props) {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    if (localStorage.getItem("user-info")) {
+      navigate("/posts");
+      console.log(props);
+    }
+  }, []);
+
+  const [formData, setFormData] = React.useState({
+    username: "",
+    password: "",
+  });
+
+  function handleFormChange(event) {
+    const { name, value } = event.target;
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let username = event.target.username.value;
+    let password = event.target.password.value;
+    let credentials = { username, password };
+
+    fetch("http://localhost:3002/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(credentials),
+    })
+      .then((result) => result.json())
+      .then((userInfo) => {
+        localStorage.setItem("user-info", JSON.stringify(userInfo));
+      });
+  }
 
   return (
-    <Form>
-      {" "}
-      <Form.Label>Username</Form.Label>
-      <Form.Control
-        type="text"
-        placeholder="Enter email"
-        name="username"
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      {/*  */}
-      <Form.Label>Password</Form.Label>
-      <Form.Control
-        type="text"
-        placeholder="Enter email"
-        name="password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      {/*  */}
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <main>
+      <Form onSubmit={handleSubmit} method="post">
+        {" "}
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter username"
+          name="username"
+          value={formData.username}
+          onChange={handleFormChange}
+        />
+        {/*  */}
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter password"
+          name="password"
+          value={formData.password}
+          onChange={handleFormChange}
+        />
+        {/*  */}
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </main>
   );
 }
