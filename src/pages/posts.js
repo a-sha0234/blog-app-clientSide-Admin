9659cc2ac680e6426a479ce6d44917aa72efb713
a-sha0234 <asharma_31@hotmail.com>
2022-05-19 14:46:44 +0100

    fetch data from endpoint

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,5 +1,21 @@
+import { react, useEffect, useState } from "react";
+
 export default function Posts(props) {
-  function isloggedIn() {}
+  let token = localStorage.getItem("user-info");
+
+  useEffect(() => {
+    fetch("http://localhost:3002/posts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    }).then((result) => {
+      return result.json().then((result) => {
+        console.log(result);
+      });
+    });
+  });
 
   return <div>sf</div>;
 }
