5f75c32393d57ff834520075cfa2d874a400be87
a-sha0234 <asharma_31@hotmail.com>
2022-05-20 21:37:24 +0100

    update

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,14 +1,12 @@
 import { react, useEffect, useState } from "react";
 
 export default function Posts(props) {
-  let token = localStorage.getItem("user-info");
-
   useEffect(() => {
     fetch("http://localhost:3002/posts", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: token,
+        Authorization: props.getToken,
       },
     }).then((result) => {
       return result.json().then((result) => {
