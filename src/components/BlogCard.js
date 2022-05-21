9e5e90130cf2c3ac3308a1e8fa6200b57900ba5d
a-sha0234 <asharma_31@hotmail.com>
2022-05-21 22:34:30 +0100

    make edit

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,14 +1,10 @@
 export default function BlogCard(props) {
-  console.log(props);
-
-  let blogDetails = "/posts/" + props.apiData._id;
+  // let blogDetails = "/posts/" + props.apiData._id;
 
   return (
     <div className="Card">
-      <a href={blogDetails}>
-        <h2>{props.apiData.title}</h2>
-        <h6>Posted on: {props.apiData.createdAt}</h6>
-      </a>
+      <h2>{props.apiData.title}</h2>
+      <h6>Posted on: {props.apiData.createdAt}</h6>
     </div>
   );
 }
